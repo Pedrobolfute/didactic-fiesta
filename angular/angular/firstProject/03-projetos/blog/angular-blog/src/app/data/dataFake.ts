7606ee1f3c87b390fe5d744f8580f85768e05a39
Pedrobolfute <pepenike-101@hotmail.com>
2024-02-25 08:20:40 -0300

    my dc blog structure

---
+++ angular/angular/firstProject/03-projetos/blog/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "photoCover":"https://files.tecnoblog.net/wp-content/uploads/2021/04/Qual-a-ordem-cronologica-dos-filmes-do-Batman-Deny-Freeman-Flickr.jpg",
+    "title":"ÁÔ BATMAN OLHANDO VOCÊ",
+    "description":"DC comprou LoremIpsum e colocou o batman pra pagar. Isso o deixou furioso porque Robin Hood lhe roubou e deu o dinheiro pro Asta de black clover.",
+  },
+  {
+    "id": 2,
+    "photoCover":"https://centralvingadores.com.br/wp-content/uploads/2019/07/S%C3%A9rie-animada-What-if.jpg.webp",
+    "title":"Marvel compra DC!",
+    "description":"ioo",
+  },
+  {
+    "id": 3,
+    "photoCover":"https://cinepop.com.br/wp-content/uploads/2017/07/dc_cinematic_universe_by_arkhamnatic-daf6emk.jpg",
+    "title":"Os protagonistas fizeram pose pra foto!",
+    "description":"ioo",
+  },
+  {
+    "id": 4,
+    "photoCover":"https://animerant.com.br/wp-content/uploads/2023/12/zoro-left-secret-one-piece.webp",
+    "title":"Zoro despertou sharingan ou olho ghoul?",
+    "description":"ioo",
+  },
+]
