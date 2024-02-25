9ffe68a014f1c299272bbbe73a57834e72e1a101
Pedrobolfute <pepenike-101@hotmail.com>
2024-02-25 17:27:44 -0300

    rotas

---
+++ angular/angular/firstProject/04-rotas/rotas/src/app/pages/portfolio/card/card.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private navegador:Router
+    ) { 
+    //http://localhost:4200/portfolio/1
+    this.activeRoute.firstChild?.params.subscribe(
+      res => console.log(res)
+    )
+
+    //http://localhost:4200/portfolio/1?name=pedro&token=123
+    this.activeRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
+
+  }
+
+  ngOnInit(): void {
+    // setInterval(()=>{
+    //   this.navegador.navigate(['/'])
+    // },5000)
+  }
+
+}
